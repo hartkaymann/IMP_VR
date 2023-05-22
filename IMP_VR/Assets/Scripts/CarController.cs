288f53e83cc9934e560c18a9c832e40596e4d004
hartkaymann <kay-hartmann@gmx.net>
2023-05-22 18:25:23 +0900

    car model

---
+++ IMP_VR/Assets/Scripts/CarController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+
+    [SerializeField] private Transform steeringWheel;
+    [SerializeField] private Transform wheelFrontLeft;
+    [SerializeField] private Transform wheelFrontRight;
+    [SerializeField] private Transform wheelRearLeft;
+    [SerializeField] private Transform wheelRearRight;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
