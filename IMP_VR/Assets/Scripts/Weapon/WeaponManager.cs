94e513f32c91d83f2138366a16f0383685bdface
Jiyoung Moon <jiyoungmoon55@gmail.com>
2023-06-02 11:45:08 +0900

    explosion end

--- IMP_VR/Assets/Scripts/Weapon/WeaponManager.cs
+++ IMP_VR/Assets/Scripts/Weapon/WeaponManager.cs
@@ -2,22 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(AudioSource))]
 public class WeaponManager : MonoBehaviour
 {
 
     // _weaponType will be Grenade or Banana
     //  Based on the tag value, the weapon effect will be changed
     private string _weaponType;    
+
+    private MeshRenderer _grenadeMesh;
+    private float _explosionDelay = 2f;
+    private float _destoryDelay = 0.5f;
+    private int _explosionRad = 4;
+    private int _explosionForce = 400;
+    [SerializeField] private GameObject _explosionEffect;
     
     void Start()
     {
         // Get the type of weapon from the tag
         _weaponType = this.gameObject.tag;
+
+
+        // Get grenade mesh
+        if (_weaponType == "Grenade")
+        {
+            _grenadeMesh = this.gameObject.GetComponent<MeshRenderer>();
+        }
         
     }
 
     // Grenade effect
     // If it is Grenade, it will explode after a few seconds of throwing and the explosion will affect to the enemy
+    public void TriggerBoom()
+    {
+        StartCoroutine(Wait());
+    }
+
+    private IEnumerator Wait() 
+    {
+        yield return new WaitForSeconds(_explosionDelay);
+        Explode();
+        _grenadeMesh.enabled = false;
+        yield return new WaitForSeconds(_destoryDelay);
+        Destroy(this.gameObject);
+    }
+
+    private void Explode()
+    {
+        //sound plays
+
+        Instantiate(_explosionEffect, transform.position, transform.rotation);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, _explosionRad);
+        foreach (Collider nearByObjects in colliders)
+        {
+            Rigidbody rb = nearByObjects.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.AddExplosionForce(_explosionForce, transform.position, _explosionRad);
+            }
+
+            
+        }
+    }
 
 
 
